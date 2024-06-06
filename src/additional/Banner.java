d6f640e19d62871257ae0982233f0911a8a5d991
Dariusz78606 <163731164+Dariusz78606@users.noreply.github.com>
2024-06-06 20:29:24 +0200

    Init
    Utworzenie projektu i struktury plików

---
+++ src/additional/Banner.java
@@ -0,0 +1,14 @@
+package additional;
+
+public class Banner {
+    private static final String BANNER = "______ _ _     _ _       _       _\n" +
+            "| ___ (_) |   | (_)     | |     | |        \n" +
+            "| |_/ /_| |__ | |_  ___ | |_ ___| | ____ _ \n" +
+            "| ___ \\ | '_ \\| | |/ _ \\| __/ _ \\ |/ / _` |\n" +
+            "| |_/ / | |_) | | | (_) | ||  __/   < (_| |\n" +
+            "\\____/|_|_.__/|_|_|\\___/ \\__\\___|_|\\_\\__,_|\n" +
+            "Autorzy: Dariusz Murawski &  Weronika Czaplinska";
+    public static String getBanner() {
+        return BANNER;
+    }
+}
